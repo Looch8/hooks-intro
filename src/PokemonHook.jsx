3ad2e31a3753f632cbcb9e7740b78524248194e9
Luke Wheldale <lukewheldale@gmail.com>
2023-09-26 19:29:12 +0930

    feat: use effect

--- src/PokemonHook.jsx
+++ src/PokemonHook.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PokemonHook() {
 	// [variable, function to set variable] = useState(initial value)
 	const [pokemon, setPokemon] = useState({});
 	const [example, setExample] = useState("example");
+
+	useEffect(() => {
+		setPokemon({ name: "Pikachu" });
+	}, []);
+
 	return (
 		<div>
-			<h1>{pokemon.name}</h1>
 			<h1>{example}</h1>
+			<h1>{pokemon.name}</h1>
 		</div>
 	);
 }
