84d4eb6f4d14b224a36ae2aac0c2073c7eb9d807
Luke Wheldale <lukewheldale@gmail.com>
2023-09-26 19:15:09 +0930

    feat: usestate implemented

--- src/PokemonHook.jsx
+++ src/PokemonHook.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 export default function PokemonHook() {
+	// [variable, function to set variable] = useState(initial value)
+	const [pokemon, setPokemon] = useState({});
+	const [example, setExample] = useState("example");
 	return (
 		<div>
-			<h1> Feature not yet implemented.</h1>
+			<h1>{pokemon.name}</h1>
+			<h1>{example}</h1>
 		</div>
 	);
 }
