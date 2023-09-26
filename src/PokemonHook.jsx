2754e40f99caf67f7b048321e9b06d908de4c5da
Luke Wheldale <lukewheldale@gmail.com>
2023-09-26 19:55:27 +0930

    feat: fetch within component did mount useEffect

--- src/PokemonHook.jsx
+++ src/PokemonHook.jsx
@@ -6,13 +6,27 @@ export default function PokemonHook() {
 	const [example, setExample] = useState("example");
 
 	useEffect(() => {
-		setPokemon({ name: "Pikachu" });
+		let fetchData = async () => {
+			let apiResponse = await fetch(
+				"https://pokeapi.co/api/v2/pokemon/25"
+			);
+			// get random pokemon
+			let apiData = await apiResponse.json();
+
+			setPokemon(apiData);
+		};
+
+		fetchData();
 	}, []);
 
-	return (
-		<div>
-			<h1>{example}</h1>
-			<h1>{pokemon.name}</h1>
-		</div>
-	);
+	if (pokemon.name) {
+		return (
+			<div>
+				<h1>{example}</h1>
+				<h1>{pokemon.name}</h1>
+			</div>
+		);
+	} else {
+		return <div>No data to show...</div>;
+	}
 }
